1f8ea155654a298557fe5067a662c1d0751de9f7
santhoshagganur <118893483+santhoshagganur@users.noreply.github.com>
2023-03-14 23:00:08 +0530

    under verification

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,1 +1,16 @@
 // Write your code here
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div className="notfound-container">
+        <h1 className="notfound-text"> Not Found </h1>
+      </div>
+    )
+  }
+}
+
+export default NotFound
