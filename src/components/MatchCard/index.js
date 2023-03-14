1547ba8e06cfca68de7b30ab7a40fa4b2e27e56d
santhoshagganur <118893483+santhoshagganur@users.noreply.github.com>
2023-03-14 23:04:28 +0530

    verification

--- src/components/MatchCard/index.js
+++ src/components/MatchCard/index.js
@@ -1,1 +1,30 @@
 // Write your code here
+// Write your code here
+import './index.css'
+
+const MatchCard = props => {
+  const {matchDetails} = props
+  const {competingTeamLogo, result, competingTeam, matchStatus} = matchDetails
+
+  const matchStatusClassname = () => {
+    if (matchStatus === 'Won') {
+      return 'match-won'
+    }
+    return 'match-lose'
+  }
+
+  return (
+    <li className="app-match-card-container">
+      <img
+        src={competingTeamLogo}
+        alt={`competing-team${competingTeam}`}
+        className="match-card-image"
+      />
+      <h1 className="match-card-heading"> {competingTeam} </h1>
+      <p className="match-card-result"> {result} </p>
+      <p className="match-won"> {matchStatus} </p>
+    </li>
+  )
+}
+
+export default MatchCard
