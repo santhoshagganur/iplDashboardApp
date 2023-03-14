f3b5395fe310427897cd5443c943d6955d8c1663
santhoshagganur <118893483+santhoshagganur@users.noreply.github.com>
2023-03-14 23:06:30 +0530

    success

--- src/components/LatestMatch/index.js
+++ src/components/LatestMatch/index.js
@@ -1,1 +1,50 @@
 // Write your code here
+import './index.css'
+import {Component} from 'react'
+
+class LatestMatch extends Component {
+  render() {
+    const {latestMatchDetails} = this.props
+    const {
+      competingTeam,
+      date,
+      venue,
+      result,
+      competingTeamLogo,
+      firstInnings,
+      secondInnings,
+      manOfTheMatch,
+      umpires,
+    } = latestMatchDetails
+
+    return (
+      <div className="latest-match-container">
+        <div>
+          <h1 className="latest-match-name"> {competingTeam} </h1>
+          <p className="latest-match-date"> {date} </p>
+          <p className="latest-match-venue"> {venue} </p>
+          <p className="latest-match-result"> {result} </p>
+        </div>
+        <div>
+          <img
+            src={competingTeamLogo}
+            className="oponent-team-logo"
+            alt={competingTeam}
+          />
+        </div>
+        <div className="match-details">
+          <h1 className="first-innings-text"> First Innings </h1>
+          <p className="first-innings-team"> {firstInnings} </p>
+          <h1 className="first-innings-text"> Second Innings </h1>
+          <p className="first-innings-team"> {secondInnings} </p>
+          <h1 className="first-innings-text"> Man of the match </h1>
+          <p className="first-innings-team"> {manOfTheMatch} </p>
+          <h1 className="first-innings-text"> Umpires </h1>
+          <p className="first-innings-team"> {umpires} </p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default LatestMatch
