335bf7dab5e2557549a77d3f2aa2807a17d20ff5
santhoshagganur <118893483+santhoshagganur@users.noreply.github.com>
2023-03-15 11:57:37 +0530

    success

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,5 +1,4 @@
 // Write your code here
-// Write your code here
 import {Component} from 'react'
 import './index.css'
 
@@ -7,7 +6,7 @@ class NotFound extends Component {
   render() {
     return (
       <div className="notfound-container">
-        <h1 className="notfound-text"> Not Found </h1>
+        <h1 className="notfound-text"> Page Not Found </h1>
       </div>
     )
   }
