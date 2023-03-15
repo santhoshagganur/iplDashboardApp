ccb28fdb81508bccb9b69a63522a6645ee3a3b1b
santhoshagganur <118893483+santhoshagganur@users.noreply.github.com>
2023-03-15 11:49:16 +0530

    success

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import './App.css'
+import {Switch, Route} from 'react-router-dom'
 import Home from './components/Home'
+import NotFound from './components/NotFound'
+import TeamMatches from './components/TeamMatches'
 
-const App = () => <Home />
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/team-matches/:id" component={TeamMatches} />
+    <Route component={NotFound} />
+  </Switch>
+)
 
 export default App
